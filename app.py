1957ce259d8023a0d00eee103679c2bf1cd1d35e
moisesini12 <moisesini2004@gmail.com>
2026-02-22 05:47:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1199,7 +1199,7 @@ if page == "📊 Dashboard":
         chart_in_card(
             "🥗 Macros recientes (14 días)",
             macros_chart,
-            height=480
+            height=380
         )
 
 # ==========================================================
@@ -2576,6 +2576,7 @@ elif page == "🤖 IA Alimento":
             st.exception(e)
 
     st.markdown("</div>", unsafe_allow_html=True)
+
 
 
 
